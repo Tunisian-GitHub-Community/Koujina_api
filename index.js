dc3be017557e8f05e63819b98d5ae0625a12cffb
ayoub3bidi <mrayoubabidi@gmail.com>
2022-01-21 16:44:44 +0100

    ✅ Adding Login & Register Routes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require ("./routes/auth")
 
 const app = express();
 dotenv.config();
+app.use(express.json());
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -12,6 +15,8 @@ mongoose
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+  app.use("/auth", authRoute);
 
 
 app.listen("5000", () => {
