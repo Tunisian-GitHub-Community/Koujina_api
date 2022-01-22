7a73620ce4082b6f8be86c7743948b91925c4fe4
ayoub3bidi <mrayoubabidi@gmail.com>
2022-01-22 13:58:00 +0100

    ✅ Adding User CRUD

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authRoute = require ("./routes/auth")
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
-const app = express();
 dotenv.config();
 app.use(express.json());
 
@@ -11,14 +12,15 @@ mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: true
+    useCreateIndex: true,
+    useFindAndModify:true
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-  app.use("/auth", authRoute);
-
+app.use("/auth", authRoute);
+app.use("/users", userRoute);
 
 app.listen("5000", () => {
-    console.log("Backend is running")
-})
+  console.log("Backend is running.");
+});
